10de818c10ffb69f7f9242964614c1901b261509
argent <argent@outlier.ai>
2020-09-21 10:18:16 +0100

    Update to Node 12

--- packer/linux/scripts/install-node.sh
+++ packer/linux/scripts/install-node.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -eu -o pipefail
 
-NODE_VERSION=8.15.0
+NODE_VERSION=12.17.0
 YARN_VERSION=1.13.0
 
 # This performs a manual install of Node.
 echo "Installing node ${NODE_VERSION}"
-sudo rpm -i https://rpm.nodesource.com/pub_8.x/el/7/x86_64/nodejs-${NODE_VERSION}-1nodesource.x86_64.rpm
+sudo rpm -i https://rpm.nodesource.com/pub_12.x/el/7/x86_64/nodejs-${NODE_VERSION}-1nodesource.x86_64.rpm
 echo "Installing yarn ${YARN_VERSION}"
 sudo npm install -g yarn@${YARN_VERSION}
 
